357f4fd615f04aa3d1c1fe4748f23afc5af76e15
Lê Trung Kiên <kienlt.dv@gmail.com>
2024-09-30 19:32:44 +0700

    Base
    Base

---
+++ TechStoresManagement/src/main/java/module-info.java
@@ -0,0 +1,13 @@
+module com.example.techstoresmanagement {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+    requires com.dlsc.formsfx;
+    requires net.synedra.validatorfx;
+    requires org.kordamp.bootstrapfx.core;
+    requires java.sql;
+    requires mysql.connector.j;
+
+    opens com.example.techstoresmanagement to javafx.fxml;
+    exports com.example.techstoresmanagement;
+}
