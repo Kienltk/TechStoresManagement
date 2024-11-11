ace6384a043f381351a28c6447d593ee2f61168d
Lyse <Lyse249@gmail.com>
2024-11-11 19:06:40 +0700

    UPDATE 11/11: MANAGER > PRODUCT VIEW

--- TechStoresManagement/src/main/java/model/DirectorModel.java
+++ TechStoresManagement/src/main/java/model/DirectorModel.java
@@ -9,7 +9,7 @@ import javafx.scene.image.ImageView;
 import java.sql.*;
 import java.util.ArrayList;
 
-public class DirectorModel implements ICommon<Product> {
+public class DirectorModel {
 
     private void productParam(PreparedStatement ps, Product product) throws SQLException {
         ps.setString(1, product.getName());
@@ -43,10 +43,6 @@ public class DirectorModel implements ICommon<Product> {
         return list;
     }
 
-    @Override
-    public ArrayList<Product> getAll(int idStore) {
-        return null;
-    }
 
     public ArrayList<Product> getAll() {
         ArrayList<Product> list = new ArrayList<>();
@@ -61,12 +57,38 @@ public class DirectorModel implements ICommon<Product> {
         return new ArrayList<>();
     }
 
-    @Override
-    public Product getOne(int idStore, long id) {
-        return null;
+    public ArrayList<Product> getAllForStore(int storeId) {
+        ArrayList<Product> list = new ArrayList<>();
+        String sql = "SELECT * " +
+                "FROM products " +
+                "JOIN products_store ON products_store.id_product = products.id " +
+                "WHERE products_store.id_store = ?";
+        try (Connection con = JDBCConnect.getJDBCConnection();
+             PreparedStatement ps = con.prepareStatement(sql)) {
+            ps.setInt(1, storeId);
+            return getProducts(list, ps);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return new ArrayList<>();
     }
 
-    @Override
+    public ArrayList<Product> getAllForWarehouse(int warehouseId) {
+        ArrayList<Product> list = new ArrayList<>();
+        String sql = "SELECT * " +
+                "FROM products " +
+                "JOIN products_warehouse ON products_warehouse.id_product = products.id " +
+                "WHERE products_warehouse.id_warehouse = ?";
+        try (Connection con = JDBCConnect.getJDBCConnection();
+             PreparedStatement ps = con.prepareStatement(sql)) {
+            ps.setInt(1, warehouseId);
+            return getProducts(list, ps);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return new ArrayList<>();
+    }
+
     public boolean add(Product obj) {
         String sql = "INSERT INTO products (product_name, purchase_price, sale_price, brand, img_address) " +
                 "VALUES (?, ?, ?, ?, ?);";
@@ -78,10 +100,6 @@ public class DirectorModel implements ICommon<Product> {
         } catch (SQLException e) {
             e.printStackTrace();
         }
-        return false;
-    }
-    @Override
-    public boolean update(Product obj, int id) {
         return false;
     }
 
@@ -98,7 +116,7 @@ public class DirectorModel implements ICommon<Product> {
         }
         return false;
     }
-    @Override
+
     public boolean delete(int productId) {
         String deleteProductSql = "DELETE FROM products WHERE id = ?;";
         String resetAutoIncrement = "ALTER TABLE products AUTO_INCREMENT = 1;";
@@ -115,6 +133,7 @@ public class DirectorModel implements ICommon<Product> {
         }
         return false;
     }
+
     public int getFinalId() {
         String sql = "SELECT MAX(id) AS max_id FROM products";
         try (Connection con = JDBCConnect.getJDBCConnection();
@@ -153,6 +172,7 @@ public class DirectorModel implements ICommon<Product> {
         }
         return false;
     }
+
     public boolean ifUniqueProductName(String productName) {
         String sql = "SELECT COUNT(*) AS count FROM products WHERE product_name =?;";
         try (Connection con = JDBCConnect.getJDBCConnection();
@@ -188,4 +208,43 @@ public class DirectorModel implements ICommon<Product> {
         }
         return 0;
     }
+
+    public int getTotalStockForStore(int productId, int storeId) {
+        String sql = "SELECT quantity " +
+                "FROM products " +
+                "JOIN products_store ON products.id = products_store.id_product " +
+                "WHERE products.id = ? AND products_store.id_store = ?;";
+        try (Connection con = JDBCConnect.getJDBCConnection();
+             PreparedStatement ps = con.prepareStatement(sql)) {
+            ps.setInt(1, productId);
+            ps.setInt(2, storeId);
+            ResultSet rs = ps.executeQuery();
+            if (rs.next()) {
+                return rs.getInt("quantity");
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return 0;
+    }
+
+    public int getTotalStockForWarehouse(int productId, int warehouseId) {
+        String sql = "SELECT quantity " +
+                "FROM products " +
+                "JOIN products_warehouse ON products.id = products_warehouse.id_product " +
+                "WHERE products.id = ? AND products_warehouse.id_warehouse = ?;";
+        try (Connection con = JDBCConnect.getJDBCConnection();
+             PreparedStatement ps = con.prepareStatement(sql)) {
+            ps.setInt(1, productId);
+            ps.setInt(2, warehouseId);
+            ResultSet rs = ps.executeQuery();
+            if (rs.next()) {
+                return rs.getInt("quantity");
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return 0;
+    }
+
 }
